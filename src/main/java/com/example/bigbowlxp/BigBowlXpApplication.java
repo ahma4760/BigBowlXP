a567e08f49d9d8d13803885a981689a3dda30783
ahma4760 <ahma4760@stud.kea.dk>
2023-10-02 11:17:56 +0200

    Initial commit

---
+++ src/main/java/com/example/bigbowlxp/BigBowlXpApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigbowlxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBowlXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigBowlXpApplication.class, args);
+    }
+
+}
